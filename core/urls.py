8fa288135298ae597a06264d020eac846cf0b772
Uxio <uxio11@gmail.com>
2016-03-05 20:22:13 +0100

    Post data example

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 
 from . import views
 urlpatterns = [
-    # ex: /polls/
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^postData/$', views.postData, name='postData'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.views import generic
+from django.views.decorators.csrf import csrf_exempt
+
+import json
 
 class IndexView(generic.TemplateView):
     template_name = 'core/index.html'
+
+@csrf_exempt
+def postData(request):
+    data = json.dumps('ok')
+    data = json.dumps(request.POST)
+    return HttpResponse(data, content_type='application/json')
