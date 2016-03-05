bb88539a8e41aefd1766f61427a3ae8c1ab559bb
Uxio <uxio11@gmail.com>
2016-03-05 20:45:06 +0100

    Make json decode for post

--- core/models.py
+++ core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Raspberry(models.Model):
-    mac = models.CharField(max_length=20)
+    mac = models.CharField(max_length=20, unique=True)
     ip = models.GenericIPAddressField()
     hostname = models.CharField(max_length=20)
 
@@ -9,3 +9,4 @@ class Data(models.Model):
     name = models.CharField(max_length=20)
     value = models.FloatField()
     device = models.ForeignKey(Raspberry, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now=True)
--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, HttpResponse
 from django.views import generic
 from django.views.decorators.csrf import csrf_exempt
 
+from .models import Raspberry, Data
+
 import json
 
 class IndexView(generic.TemplateView):
@@ -9,6 +11,23 @@ class IndexView(generic.TemplateView):
 
 @csrf_exempt
 def postData(request):
+    jsonData = json.loads(request.body.decode())
+    device = jsonData['device']
+    mac = device['mac']
+    hostname = device['hostname']
+    ip = request.META.get('REMOTE_ADDR')
+    try:
+        raspberry = Raspberry.objects.get(mac=mac)
+    except:
+        print('New mac detected, creating raspberry')
+        raspberry = Raspberry(mac=mac, ip=ip, hostname=hostname)
+        raspberry.save()
+
+    data = jsonData['data']
+    name = data['name'].upper()
+    value = data['value']
+    d = Data(name=name, value=value, device=raspberry)
+    d.save()
+
     data = json.dumps('ok')
-    data = json.dumps(request.POST)
     return HttpResponse(data, content_type='application/json')
